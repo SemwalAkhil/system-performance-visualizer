87b601ee04e2355d0cb2fa468f8327e501314109
Akhil Semwal <akhil392semwal@gmail.com>
2026-02-01 22:46:22 +0530

    Add stress test for LinuxSystemMonitor

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
 #include "core/system/LinuxSystemMonitor.h"
+#include <thread>
+#include <chrono>
 
 int main()
 {
     LinuxSystemMonitor monitor;
+    while (true)
+    {
+        std::cout << "Memory Usage: "
+                  << monitor.getMemoryUsage()
+                  << "%\n";
 
-    std::cout << "Memory Usage: "
-              << monitor.getMemoryUsage()
-              << "%\n";
+        std::cout << "CPU Usage: "
+                  << monitor.getCPUUsage()
+                  << "%\n";
 
-    std::cout << "CPU Usage: "
-              << monitor.getCPUUsage()
-              << "%\n";
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
 
     return 0;
 }
